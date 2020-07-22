f05660381d0651fcbbb09b490bc0fabff3d85d55
ouyuming <471246938@qq.com>
2020-07-22 18:08:42 +0800

    完成将文件名称和路径写入文件的功能

--- imageCut.py
+++ imageCut.py
@@ -9,22 +9,23 @@ import math
 
 
 # 未处理图片的存放路径
-JpgPath = r'C:/Users/Administrator/Desktop/VOCdevkit/VOC2007/JPEGImages/'
+JpgPath = r'C:/Users/Administrator/Desktop/pyProject/file/VOCdevkit/VOC2007/JPEGImages/'
 # 处理后图片的存放路径
-ProcessedPath = r'C:/Users/Administrator/Desktop/exercise/'
+ProcessedPath = r'C:/Users/Administrator/Desktop/pyProject/file/exercise/'
 #xml的存放路径
-AnnoPath = r'C:/Users/Administrator/Desktop/VOCdevkit/VOC2007/Annotations/'
+AnnoPath = r'C:/Users/Administrator/Desktop/pyProject/file/VOCdevkit/VOC2007/Annotations/'
 
 #创建一个txt文件,文件名为mytxtfile,并向文件写入msg
-def txt_creat(name,msg):
+def txt_creat(name,path):
     print('俺进来这个方法啦！')
-    desktop_path = "C:\\Users\\Administrator\\Desktop\\"
-    full_path = desktop_path + name + '.txt'
-    file = open(full_path,'w')
+    desktop_path = "C:\\Users\\Administrator\\Desktop\\pyProject\\file\\"
+    full_path = desktop_path + 'voc.txt'
+    file = open(full_path,'a+') #新的内容将会被写入到已有内容之后
+    msg = path + ":" + name + "\n"
     file.write(msg)
 
+
 def get_image(file_name):
-    print('测试')
     #获取图片
     image_name, ext = os.path.splitext(file_name) #分割路径中的文件名与拓展名
     imgfile = JpgPath + file_name #拼接图片的路径，找到指定路径
@@ -35,7 +36,7 @@ def get_image(file_name):
     annotation = DomTree.documentElement
     objectlist = annotation.getElementsByTagName('object')
 
-    #切割图片
+    #切割图片(利用for循环)
     i = 0
     for objects in objectlist:
         namelist = objects.getElementsByTagName('name')
@@ -44,6 +45,7 @@ def get_image(file_name):
         if not os.path.exists(savepath): #路径不存在则创建路径
             os.makedirs(savepath)
         bndboxs = objects.getElementsByTagName('bndbox') #通过bndbox标签获取切割四个坐标
+        x1_list = bndboxs[0].getElementsByTagName('xmin')
         x1_list = bndboxs[0].getElementsByTagName('xmin')
         x1 = math.ceil(float(x1_list[0].childNodes[0].data)) #获取x轴最小坐标
         y1_list = bndboxs[0].getElementsByTagName('ymin')
@@ -57,20 +59,21 @@ def get_image(file_name):
         cropedimg = img.crop(crop_box) #根据数组坐标裁剪图片(pillow模块Image.crop()函数切割图片)
         i += 1 #每张图片里面，对象的个数
         print(str(i))
-        cropedimg.save(savepath + '/' + image_name + '_' + str(i) + '.jpg') #保存在给定文件名下的该图像
-        #将图片的路径和类型保存到txt
-        
+        allPath = savepath + '/' + image_name + '_' + str(i) + '.jpg'
+        cropedimg.save(allPath) #保存在给定文件名下的该图像
+        #C:/Users/Administrator/Desktop/exercise/aeroplane/000067_1.jpg
+        #将图片的路径和类型保存到txt;image_name是图片类型;
+        txt_creat(objectname, allPath)
 
 
 
 
 if __name__ == '__main__':
     image_list = os.listdir(JpgPath)  # 拿到JpgPath路径下的所有图片
-    #需要理解透这句代码的执行原理
+    #需要理解透这句代码的执行原理(这个多线程貌似控制不太好)
     with concurrent.futures.ProcessPoolExecutor() as executor:
         executor.map(get_image, image_list)
     print('图片获取完成 。。。！')
-    print('ceshi')
 
 
 
