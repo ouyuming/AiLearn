e7e74cbf75585834a4dfe30f10bb2faf3f559431
ouyuming <471246938@qq.com>
2020-07-23 08:55:25 +0800

    完成将文件名称和路径写入文件的功能

--- saveToTxt.py
+++ saveToTxt.py
@@ -2,9 +2,14 @@ from __future__ import division
 
 #创建一个txt文件,文件名为mytxtfile,并向文件写入msg
 def txt_creat(name,msg):
-    desktop_path = ""
+    print('俺进来这个方法啦！')
+    desktop_path = "C:\\Users\\Administrator\\Desktop\\"
+    full_path = desktop_path + name + '.txt'
+    file = open(full_path,'w')
+    file.write(msg)
+
 
 
 
 if __name__ == '__main__':
-    txt_creat('mytxtfile', 'msg')
+    txt_creat('mytxtfile', '我在测试文件')
